e76c7fb064ecfd1b103750f06c49740b2b589e37
Izzan Nuruddin <izzan@kata.ai>
2020-02-11 16:36:04 +0700

    add fetch function and add getInitialProps which like componentDidMount

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,12 +1,14 @@
-import Link from 'next/link';
+import Link from 'next/link'
 
 function About() {
-    return (
-       <div>
-          <p className="col-lg-6">Cuma ini kok</p>
-          <Link href="/"><a className="col-lg-6">Balik aja yuk</a></Link>
-       </div>
-    )
+  return (
+    <div>
+      <p className="col-lg-6">Cuma ini kok</p>
+      <Link href="/">
+        <a className="col-lg-6">Balik aja yuk</a>
+      </Link>
+    </div>
+  )
 }
 
-export default About;
+export default About
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,27 @@
-import Link from 'next/link';
+import Link from 'next/link'
+import fetch from 'isomorphic-unfetch'
 
-function Index() {
-   return(
-      <div>
-         <p className="col-md-2">Halo Dunia!</p>
-         <Link href="/about"><a className="col-lg-6">Kesini yuk</a></Link>
-      </div>
-   )
+function Index({ stars }) {
+  return (
+    <div>
+      <p className="col-md-2">Halo Dunia!</p>
+      <Link href="/about">
+        <a className="col-lg-6">Kesini yuk</a>
+      </Link>
+      <p>{stars}</p>
+    </div>
+  )
 }
 
-export default Index;
+Index.getInitialProps = async ctx => {
+  const res = await fetch('https://api.github.com/repos/zeit/next.js')
+  const json = await res.json()
+  /**
+   * `ctx` return `pathname`, `query`, `asPath`, and `AppTree`
+   * so you can get pathname(URL), query/params from URL, and custom path from asPath
+   */
+  console.log(ctx)
+  return { stars: json.stargazers_count }
+}
+
+export default Index
